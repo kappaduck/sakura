618a0c7c9443cfa7fe8d44c9aa2c20ed828cd843
beauchama <beauchama@kappaduck.com>
2025-03-30 19:45:27 -0400

    Remove SDLNative.Memory

--- src/KappaDuck.Sakura/Interop/SDL/SDLNative.cs
+++ src/KappaDuck.Sakura/Interop/SDL/SDLNative.cs
@@ -1,9 +1,24 @@
 // Copyright (c) KappaDuck. All rights reserved.
 // The source code is licensed under MIT License.
 
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 namespace KappaDuck.Sakura.Interop.SDL;
 
 internal static partial class SDLNative
 {
     internal const string LibraryName = "SDL3";
+
+    internal static unsafe void Free<T>(T* memory) where T : unmanaged
+    => Free((nint)memory);
+
+    internal static unsafe void Free<T>(T** memory) where T : unmanaged
+        => Free((nint)memory);
+
+    internal static void Free(nint memory) => SDL_free(memory);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial void SDL_free(nint memory);
 }
